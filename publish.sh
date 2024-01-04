005c8c4d1560f94e6ae049a5d2e534289e1190b4
monlor <me@monlor.com>
2024-01-04 16:44:33 +0800

    feat: 🎸 支持保存历史的package版本

--- publish.sh
+++ publish.sh
@@ -4,10 +4,12 @@ set -e
 
 helm lint charts/*
 
-rm -rf ./*.tgz
+# rm -rf ./*.tgz
 
-helm package charts/*
+helm package charts/*  --destination package
 
-helm repo index --url https://monlor.github.io/helm-charts/ .
+helm repo index --url https://monlor.github.io/helm-charts/package/ package
+
+mv -f package/index.yaml .
 
 cat index.yaml
